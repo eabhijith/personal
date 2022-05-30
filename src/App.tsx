c49438ff06018aef7ffe72bdb65c18ae07781053
Abhijith Eanuga <abhijith.eanuga@zalando.de>
2022-05-30 14:19:42 +0200

    including flexi  boxes

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,12 @@ function App() {
           Hello world!
         </h1>
 
+        <div className="flex flex-row">
+          <div className="basis-1/4">01</div>
+          <div className="basis-1/4">02</div>
+          <div className="basis-1/2">03</div>
+        </div>
+
       </header>
     </div>
   );
