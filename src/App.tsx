52231445ddd8735882b513451b9fa1bf553122b9
Abhijith Eanuga <abhijith.eanuga@zalando.de>
2022-05-30 14:23:17 +0200

    adding tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div >
+      <header >
         Sample output..
         <h1 className="text-3xl font-bold underline">
           Hello world!
@@ -15,6 +15,23 @@ function App() {
           <div className="basis-1/4">02</div>
           <div className="basis-1/2">03</div>
         </div>
+
+
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+  <img className="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
+  <div className="px-6 py-4">
+    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p className="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div className="px-6 pt-4 pb-2">
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
+
 
       </header>
     </div>
