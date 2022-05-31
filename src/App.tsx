053c7e088c9c743d275f0f56a1775b06aef10f80
Abhijith Eanuga <abhijith.eanuga@zalando.de>
2022-05-31 09:48:11 +0200

    updated changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,229 @@
-import React from 'react';
+import * as React from 'react';
 import './App.css';
+import Header from './Components/Header';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider  } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import { red } from '@mui/material/colors';
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+
+const themeLight = createTheme({
+  palette: {
+    background: {
+      default: "linear-gradient(230deg, #838c4f4a 2.7%, #ded5d521 100%)"
+    }
+  }
+});
+
+const themeDark = createTheme({
+  palette: {
+    background: {
+      default: "skyblue"
+    }
+  }
+});
+
+
+
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+
+        palette: {
+          mode : prefersDarkMode ? 'dark' : 'light',
+          ...(prefersDarkMode 
+              ?{
+                background: {
+                  default: "#16375a",
+                }
+              }
+              :{
+                background: {
+                  default: "gray",
+                }
+              }
+          ),
+          // mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
   return (
     <div >
-      <header >
-        Sample output..
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+      <Header/>
+      {/* <div className='container'>
+        <div className='item'>1</div>
+        <div className='item'>2</div>
+        <div className='item'>3</div>
 
-        <div className="flex flex-row">
-          <div className="basis-1/4">01</div>
-          <div className="basis-1/4">02</div>
-          <div className="basis-1/2">03</div>
-        </div>
+      </div>
+
+      <div className='container'>
+        <div className='item'>3</div>
+        <div className='item'>4</div>
+      </div> */}
+
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          p: 1,
+          m: 1,
+          bgcolor: 'background.default',
+          borderRadius: 1,
+        }}
+      >
+
+        <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+              <CardMedia
+                  component="img"
+                  height="140"
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  alt="green iguana"
+                />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              </CardActionArea>
+            </Card>
+
+      </Box>
+
+      
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          p: 1,
+          m: 1,
+          bgcolor: 'background.default',
+          borderRadius: 1,
+        }}
+      >
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+              <CardMedia
+                  component="img"
+                  height="140"
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  alt="green iguana"
+                />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              </CardActionArea>
+            </Card>
+
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+              <CardMedia
+                  component="img"
+                  height="140"
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  alt="green iguana"
+                />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              </CardActionArea>
+            </Card>
 
 
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-  <img className="w-full" src="https://images.unsplash.com/photo-1600703136783-bdb5ea365239?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVkJTIwZmxvd2VyfGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="Sunset in the mountains"/>
-  <div className="px-6 py-4">
-    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-    <p className="text-gray-700 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-    </p>
-  </div>
-  <div className="px-6 pt-4 pb-2">
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-  </div>
-</div>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+              <CardMedia
+                  component="img"
+                  height="140"
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  alt="green iguana"
+                />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              </CardActionArea>
+            </Card>
+
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+              <CardMedia
+                  component="img"
+                  height="140"
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  alt="green iguana"
+                />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              </CardActionArea>
+            </Card>
+        <Item>Item 2</Item>
+        <Item>Item 3</Item>
+        <Item>Item 4</Item>
+        <Item>Item 5</Item>
+        <Item>Item 6</Item>
+      </Box>
+    </ThemeProvider>
 
 
-      </header>
+
+
     </div>
   );
 }
