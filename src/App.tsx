4fef797b2401111f892a7b3647de415ccf080f45
Abhijith Eanuga <abhijith.eanuga@zalando.de>
2022-05-31 16:34:19 +0200

    updated changes

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,13 @@ import Navbar from './Components/Navbar/Navbar';
 import {  Route, Routes} from "react-router-dom";
 import Home from './Components/Home/Home';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+// }));
 
 
 
