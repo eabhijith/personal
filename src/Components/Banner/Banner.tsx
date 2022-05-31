f3692e3bb30d49eb98ea590f56b85382931905f9
Abhijith Eanuga <abhijith.eanuga@zalando.de>
2022-05-31 20:29:29 +0200

    updated changes

--- src/Components/Banner/Banner.tsx
+++ src/Components/Banner/Banner.tsx
@@ -2,13 +2,9 @@ import { Component} from 'react'
 // import 'bulma/css/bulma.min.css';
 import  "./Banner.css";
 
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { shadows } from '@mui/system';
 
 interface BannerProps {
 
